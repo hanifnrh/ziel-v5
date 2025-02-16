14201ed21427c54cef4cfa40f1ccffcbf7f77fbb
hanirnrh <nurrahmathaniff@gmail.com>
2025-02-16 19:58:13 +0700

    Allow all user agent robots ts

--- app/robots.ts
+++ app/robots.ts
@@ -4,6 +4,10 @@ export default function robots(): MetadataRoute.Robots {
     return {
         rules: [
             {
+                userAgent: "*",
+                allow: ["/"],
+            },
+            {
                 userAgent: ["Googlebot", "Applebot", "Bingbot"],
                 allow: ["/"],
             },
