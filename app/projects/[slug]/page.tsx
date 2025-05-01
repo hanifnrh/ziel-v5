8db728f819ad7cffbe6c97495919aa3e2367a512
hanirnrh <nurrahmathaniff@gmail.com>
2025-05-01 15:00:18 +0700

    Change CDN to local

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -4,6 +4,7 @@ import { Loader } from '@/components/ui/loader';
 import { RichText } from '@graphcms/rich-text-react-renderer';
 import { RichTextContent } from '@graphcms/rich-text-types';
 import { Dot, MoveLeft, Paperclip } from 'lucide-react';
+import { NextSeo } from 'next-seo';
 import Image from "next/image";
 import Link from 'next/link';
 import { useParams } from "next/navigation";
@@ -19,61 +20,6 @@ interface Post {
 }
 
 const HYGRAPH_ENDPOINT = process.env.NEXT_PUBLIC_HYGRAPH_URL!;
-
-async function getPost(slug: string): Promise<Post | null> {
-    const response = await fetch(HYGRAPH_ENDPOINT, {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${process.env.NEXT_PUBLIC_HYGRAPH_TOKEN}`
-        },
-        body: JSON.stringify({
-            query: `
-                query GetProjectBySlug($slug: String!) {
-                    project(where: { slug: $slug }) {
-                        title
-                        description
-                        content { json }
-                        publishDate
-                        featuredImage { url }
-                        tag
-                    }
-                }
-            `,
-            variables: { slug },
-        }),
-        cache: "no-store", // Menghindari cache untuk mendapatkan data terbaru
-    });
-
-    const json = await response.json();
-    return json?.data?.project || null;
-}
-
-export async function generateMetadata({ params }: { params: { slug: string } }) {
-    const post = await getPost(params.slug);
-
-    return {
-        title: post?.title || "Projects",
-        description: post?.description || "View my latest projects.",
-        openGraph: {
-            type: "article",
-            url: `https://ziel.works/projects/${params.slug}`,
-            title: post?.title || "Projects",
-            description: post?.description || "View my latest projects.",
-            images: [
-                {
-                    url: post?.featuredImage?.url || "/thumbnail.png",
-                    width: 1200,
-                    height: 630,
-                    alt: post?.title,
-                },
-            ],
-        },
-        twitter: {
-            cardType: "summary_large_image",
-        },
-    };
-}
 
 export default function ProjectDetail() {
     const params = useParams();
@@ -152,6 +98,28 @@ export default function ProjectDetail() {
 
     return (
         <>
+            <NextSeo
+                title={post?.title || "Projects"}
+                description={post?.description || "View my latest projects."}
+                openGraph={{
+                    type: "article",
+                    url: `https://ziel.works/projects/${slug}`,
+                    title: post?.title || "Projects",
+                    description: post?.description || "View my latest projects.",
+                    images: [
+                        {
+                            url: post?.featuredImage?.url || "https://zielbucket.s3.ap-southeast-2.amazonaws.com/public/images/thumbnail.png",
+                            width: 1200,
+                            height: 630,
+                            alt: post?.title,
+                        },
+                    ],
+                }}
+                twitter={{
+                    cardType: "summary_large_image",
+                }}
+            />
+
             <Image
                 className="absolute top-0 z-0 -translate-y-1/2"
                 src="/bg-back.png"
@@ -173,7 +141,7 @@ export default function ProjectDetail() {
 
                     <div className="">
                         <Image
-                            src={post?.featuredImage?.url || "/thumbnail.png"}
+                            src={post?.featuredImage?.url || "https://zielbucket.s3.ap-southeast-2.amazonaws.com/public/images/thumbnail.png"}
                             width={1000}
                             height={600}
                             className="w-full aspect-[5/2] sm:aspect-[5/1] object-cover rounded-sm"
