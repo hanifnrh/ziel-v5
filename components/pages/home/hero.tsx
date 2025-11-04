e92be1d081d4f7c1a67e97de12f51a864fdc17fc
hanirnrh <nurrahmathaniff@gmail.com>
2025-11-04 17:49:42 +0700

    Update hero ui

--- components/pages/home/hero.tsx
+++ components/pages/home/hero.tsx
@@ -19,7 +19,7 @@ export default function Hero() {
                         initial={{ opacity: 0, x: -50 }}
                         whileInView={{ opacity: 1, x: 0 }}
                         transition={{ delay: 0.4, duration: 0.8 }}
-                        className="w-full flex flex-col items-start justify-center gap-4 md:px-0 sm:col-span-4 order-2 sm:order-1">
+                        className="w-full flex flex-col items-start justify-center gap-2 sm:gap-8 md:px-0 sm:col-span-4 order-2 sm:order-1">
                         <h2 className="body-bold sm:body text-4xl lg:text-7xl text-left tracking-tighter bg-gradient-to-b from-neutral-900 via-neutral-700 to-neutral-600/70 bg-clip-text text-transparent flex items-center gap-4 sm:gap-8">
                             Purpose Driven
                             <Image
@@ -30,7 +30,7 @@ export default function Hero() {
                                 className="h-8 lg:h-full w-auto inline-block align-middle mx-2"
                             />
                         </h2>
-                        <div className="flex items-start gap-4 sm:gap-8">
+                        <div className="flex items-start gap-2 sm:gap-8">
                             <Image
                                 src="/images/ideas.png"
                                 alt="ideas"
@@ -39,7 +39,7 @@ export default function Hero() {
                                 className="h-8 lg:h-full w-auto inline-block align-middle mx-2"
                             />
                             <div className="flex flex-col gap-4">
-                                <h2 className="body-bold sm:body text-4xl lg:text-7xl text-left tracking-tighter inline-flex gap-4">
+                                <h2 className="body-bold sm:body text-4xl lg:text-7xl text-left tracking-tighter inline-flex gap-2">
                                     <span className="bg-gradient-to-b from-neutral-900 via-neutral-700 to-neutral-600/70 bg-clip-text text-transparent">
                                         Product
                                     </span>
